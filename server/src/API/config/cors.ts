9576014b187dcfd030e8ef3727a99446b2b37176
Miller Kodish <millerkodish25@gmail.com>
2024-12-08 16:46:10 -0500

    Added Rate Limiting

--- server/src/API/config/cors.ts
+++ server/src/API/config/cors.ts
@@ -4,6 +4,7 @@
  */
 
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 /**
  * CORS configuration for the application.
@@ -29,4 +30,24 @@ const corsConfig = cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Authorization'], // Allowed request headers
 });
 
-export default corsConfig;
+const rateLimitValue = parseInt(process.env.RATE_LIMIT || '100', 10);
+console.log(`Rate limiting set to ${rateLimitValue.toLocaleString()} requests per 15 minutes.`);
+
+
+/**
+ * Rate limiting middleware.
+ * Limits the number of requests per IP to prevent abuse.
+ * 
+ * - **windowMs**: Time frame for which requests are checked (15 minutes).
+ * - **max**: Maximum number of requests allowed per IP within the time frame (100 requests).
+ * - **message**: Custom error message returned when the rate limit is exceeded.
+ * 
+ * @constant {Function}
+ */
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: rateLimitValue, // Limit each IP to the defined number of requests per windowMs
+  message: 'Too many requests, please try again later.' // Use a string instead of an object
+});
+
+export { corsConfig, rateLimiter };
